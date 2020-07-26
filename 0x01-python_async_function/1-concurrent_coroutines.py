3fbb3539c639495d359c92a92a79a3c67c832100
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-07-26 18:49:14 -0500

    Let's execute multiple coroutines with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,31 +1,19 @@
 #!/usr/bin/env python3
-'''
+
+"""
 1. Let's execute multiple coroutines at the same time with async
-'''
+"""
 
 import asyncio
+import random
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''
-    Async routine called wait_n that takes in 2 int arguments: max_delay and n.
-    You will spawn wait_random n times with the specified max_delay.
-    wait_n should return the list of all the delays (float values).
-    The list of the delays should be in ascending order without using
-    sort() because of concurrency.
-    '''
-    tasks = []
-    delays = []
-
-    for _ in range(n):
-        tasks.append(asyncio.create_task(wait_random(max_delay)))
-
-    # Create queue with results depending on the function have the result ready
-    # Read more: shorturl.at/grAUY
-    for task in asyncio.as_completed(tasks):
-        delays.append(await task)
-
-    return delays
+    """
+    Spawns wait_random n times with max_delay.
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [delays.append(await task) for task in asyncio.as_completed(tasks)]
