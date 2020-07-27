72bd72a818b03793cf920da74fe7c41b9c2cf550
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-07-26 22:16:22 -0500

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-"""Basic annotations task 1"""
+'''
+Basic annotations task 1
+'''
 
 
 def add(a: float, b: float) -> float:
-    """Calculates the sum of two floats
-    Args:
-        a: float to sum
-        b: float to sum
+    '''
+    Calculates the sum of two floats
     Returns: float, sum of a and b
-    """
-    return a + b
-    
+    '''
+    return a + b    
