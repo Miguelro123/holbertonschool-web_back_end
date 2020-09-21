fdaee2c8485c67f690282bda15675a7e13c7f700
Miguelro123 <51714447+Miguelro123@users.noreply.github.com>
2020-09-21 07:52:14 -0500

    Airport

--- 0x10-ES6_classes/7-airport.js
+++ 0x10-ES6_classes/7-airport.js
@@ -1,13 +1,10 @@
-const util = require('util');
-
 export default class Airport {
   constructor(name, code) {
-    if (typeof name === 'string') this._name = name;
-    if (typeof code === 'string') this._code = code;
+    this._name = name;
+    this._code = code;
   }
 
-  // custom string representation of Airport
-  [util.inspect.custom]() {
-    return `${this.constructor.name} [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
   }
 }
